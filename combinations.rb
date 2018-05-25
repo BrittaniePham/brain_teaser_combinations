5a5a6ad9f6be627944e6c6ccf05dcce687d26e85
Brittanie Pham <brittaniepham@gmail.com>
2018-05-25 13:38:19 -0600

    initial commit

---
+++ combinations.rb
@@ -0,0 +1,18 @@
+#ask for user input and output all possible combinations
+#clayton's solution in class
+
+def brain_teaser(word)
+  a = 0
+  b = 0
+  while a < word.length
+    b = a
+    while b < word.length
+      print (word[a..b])
+      print (", ")
+      b += 1
+    end
+    a += 1
+  end
+end
+
+brain_teaser("dog")
